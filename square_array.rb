b31e064173857d2af696fa996fec2a7585dcf991
Tharon Weighill <tharonweighill@gmail.com>
2020-07-30 22:25:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,7 @@
 def square_array(array)
   new_numbers = []
   array.each do |n|
-  
-  puts "The number is #{n}"
-new_numbers.push(n * n)
-
-
-end
-
-
-
-
-
-
+    puts "The number is #{n}"
+  new_numbers.push(n * n)
+  end
 end
